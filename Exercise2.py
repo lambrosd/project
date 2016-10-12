9ae964968cef50ab1596861e6c8a3efdccfed9a8
lambrosd <labros.daik@gmail.com>
2016-10-12 20:08:09 +0300

    Exercise 2 - python part 1

---
+++ Exercise2.py
@@ -0,0 +1,16 @@
+sum=0
+input_1 = int(input("Enter number of 50 euro banknotes: "))
+count_1 = 50*input_1
+input_2 = int(input("Enter number of 20 euro banknotes: "))
+count_2 = 20*input_2
+input_3 = int(input("Enter number of 10 euro banknotes: "))
+count_3 = 10*input_3
+input_4 = int(input("Enter number of 5 euro banknotes: "))
+count_4 = 5*input_4
+input_5 = int(input("Enter number of 2 euro coins: "))
+count_5 = 2*input_5
+input_6 = int(input("Enter number 1 euro coints: "))
+count_6 = 1*input_6
+sum = int(count_1+count_2+count_3+count_4+count_5+count_6)
+print('You have',sum, 'euros.')
+
